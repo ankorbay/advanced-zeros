366c2ad6766b2bfac343146a47d78c24aede840c
antonko <anton.m.korshunov@gmail.com>
2018-10-03 10:20:23 +0300

    Adv. Zeros task(#8) Solution (87%test)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(number,base) {
+  let counter = 0;
+  const prime=getMultipliers(base);
+  const len = prime.length;
+  const lastPrime = prime[len-1]
+  const numOfsimilMult=prime.filter(num=>num===lastPrime).length;
+  while (number >= lastPrime)
+  {
+    number = Math.floor(number / lastPrime);
+    counter += number;
+  }
+  // return Math.floor(counter);
+  return Math.floor(counter/numOfsimilMult);
 }
+
+function getMultipliers(number){
+  let result = [];
+  let curNum = number;
+  let probe = 2; 
+  
+  while (curNum!==1){
+      if (curNum%probe !== 0){
+          probe++;
+          }
+      else {
+          curNum /= probe
+          result.push(probe);
+          }
+      }
+  return result
+  }
+  //console.log(getZerosCount(60559525, 147));
